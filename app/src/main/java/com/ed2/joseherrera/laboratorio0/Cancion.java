8f36bbb4385d76845753b9939b20f8f6e52de5bf
Jose Herrera <31715468+joseherrera7@users.noreply.github.com>
2018-08-27 19:13:57 -0600

    Avances!

---
+++ app/src/main/java/com/ed2/joseherrera/laboratorio0/Cancion.java
@@ -0,0 +1,22 @@
+package com.ed2.joseherrera.laboratorio0;
+
+public class Cancion {
+    private String Nombre;
+    private int Duracion;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getDuracion() {
+        return Duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        Duracion = duracion;
+    }
+}
--- app/src/main/java/com/ed2/joseherrera/laboratorio0/MainActivity.java
+++ app/src/main/java/com/ed2/joseherrera/laboratorio0/MainActivity.java
@@ -3,8 +3,10 @@ package com.ed2.joseherrera.laboratorio0;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Dictionary;
 
+public class MainActivity extends AppCompatActivity {
+    Dictionary<String, Cancion> nuevoDiccionario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
